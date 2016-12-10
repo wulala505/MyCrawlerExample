eb3889b8fa2a49d5c12ea71b53098dd47dbe89be
wulala505 <wulala505@gmail.com>
2016-12-10 16:45:13 +0800

    FacebookExam

--- src/crawler/example/exam/FacebookExam.java
+++ src/crawler/example/exam/FacebookExam.java
@@ -24,27 +24,29 @@ public class FacebookExam {
 		// 遠端資料路徑
 
 		String uri = 
-				"https://graph.facebook.com/v2.6"
-				+ "/search?q="
-				+ "&access_token=";
-
+				"https://graph.facebook.com/v2.8"
+				+ "/crazyck101/posts?fields=id,link,message,created_time,reactions.type(HAHA).limit(0).summary(total_count)&since=1480824365&until=1480856400&locale=zn_tw"
+				+ "&access_token=EAACEdEose0cBAKlW9iWmaTPsgbnDwIPiF7PZCUPd3pIti4RcgGrIPi5MHv8lQT0SZCFnaIyjFP0IUP38UxlT2lvM5yVEozDgPsUIrPP36jGGHT4SYRP58atOwWBzeznxzauhmDxs3Stw1J7PYZAvq6BpLVQv3SSgYlh33N10QZDZD";
+		//
 
 		Elements elems =
 				CrawlerPack.start()
 				.getFromJson(uri)
 				.select("data");
 		
-		String output = "id,reactions";
+		String output = "id,created_time,reactions";
 
 		// 遂筆處理
 		for( Element data: elems ){
 			String id = data.select("id").text();
-
+			String created_time = data.select("created_time").text();
+			String reactions = data.select("reactions total_count").text();
+			// css selector (空白:底下皆選(any))
 			// FIXIT
-			String reactions = "";
+			//String reactions = "haha";
 
 
-			output += id + "," + reactions + "\n";
+			output += id + "," + created_time + "," + reactions + "\n";
 		}
 
 		System.out.println( output );
